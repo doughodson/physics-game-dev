5bc1c117464fe84c2a79fe9826084a8eaa9597b8
Doug Hodson <doug@sidechannel.net>
2020-10-29 14:43:28 -0400

    update

--- ch15-flight-sim/main.cpp
+++ ch15-flight-sim/main.cpp
@@ -1,6 +1,4 @@
 
-
-
 #include <iostream>
 #include <cstdio>
 
@@ -19,15 +17,24 @@ int main()
    const float dt{1.0f / static_cast<float>(FRAME_RATE)};
    float current_time{};
 
-   for(int i{}; i < 10; i++) {
+   ZeroRudder();
+   ZeroAilerons();
+   ZeroElevators();
+   ZeroFlaps();
+
+   for (int i{}; i < 10; i++) {
+
+      IncThrust();
+
       StepSimulation(dt);
       std::cout << "Simulation time : " << current_time << std::endl;
 
       std::cout << "Roll   : " << Airplane.vEulerAngles.x << std::endl;
-      std::cout << "Pitch  : " << Airplane.vEulerAngles.y << std::endl;
+      std::cout << "Pitch  : " << -Airplane.vEulerAngles.y << std::endl;
       std::cout << "Yaw    : " << Airplane.vEulerAngles.z << std::endl;
       std::cout << "Alt    : " << Airplane.vPosition.z << std::endl;
       std::cout << "Thrust : " << ThrustForce << std::endl;
+      std::cout << "Speed  : " << Airplane.fSpeed / 1.688 << std::endl; // divide by 1.688 to convert ft/s to knots
 
       if (Flaps) std::cout << "Flaps!\n";
       if (Stalling) std::cout << "Stall!\n";
